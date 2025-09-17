aed4ec8370e29b938b9e077632fd237f5f727ff8
Martins John <martinsjohn2006gmail.com>
2025-09-17 02:59:21 +0100

    fix an indentation issue in settings.py line 91

--- app/core/settings.py
+++ app/core/settings.py
@@ -87,15 +87,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-
- DATABASES = {
-     'default': {
+DATABASES = {
+    'default': {
          'ENGINE': 'django.db.backends.postgresql',
          'HOST': os.environ.get('DB_HOST'),
          'NAME': os.environ.get('DB_NAME'),
          'USER': os.environ.get('DB_USER'),
          'PASSWORD': os.environ.get('DB_PASS'),
-     }
+     },
 }
 
 
